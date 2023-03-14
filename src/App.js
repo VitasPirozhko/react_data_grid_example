911998ff251fc51a75d16aca17d6485c5103f2dc
Вітас Пірожко <vitaspirozko@Vitass-Air.Dlink>
2023-03-14 17:45:00 +0200

    upload cvs and render data

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import 'react-data-grid/lib/styles.css';
 import DataGrid from 'react-data-grid';
+import CSVReader from "react-csv-reader";
+import { useState } from 'react';
 
-const columns = [
-  { key: 'id', name: 'ID' },
-  { key: 'title', name: 'Title' },
-];
 
-const rows = [
-  { id: 0, title: 'Example' },
-  { id: 1, title: 'Demo' },
-];
+const papaparseOptions = {
+  header: true,
+  dynamicTyping: true,
+  skipEmptyLines: true,
+  transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
+};
 
 function App() {
+  const [rows, setRows] = useState([]);
+
+  const handleForce = (data, fileInfo) => {
+    setRows(data)
+  };
+
+  const columns = Object.keys(rows[0] || {}).map((key) => {
+    return {
+      key,
+      name: key
+    }
+  })
+  console.log(rows);
   return (
-    <div>
-       <DataGrid columns={columns} rows={rows} />;
-    </div>
+    <>
+      <div style={{marginBottom: "50px"}}>
+        <CSVReader
+          onFileLoaded={handleForce}
+          parserOptions={papaparseOptions}
+        />
+      </div>
+      <div>
+        <DataGrid columns={columns} rows={rows} />;
+      </div>
+    </>
   );
 }
 
