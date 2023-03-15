1dc21b9335fa1c00a3e51e1d1e8a331344cc6499
Вітас Пірожко <vitaspirozko@Vitass-Air.Dlink>
2023-03-15 13:34:32 +0200

    change plugin

--- src/App.js
+++ src/App.js
@@ -1,40 +1,18 @@
-import 'react-data-grid/lib/styles.css';
-import DataGrid from 'react-data-grid';
-// import CSVReader from "react-csv-reader";
-// import { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
 import { mockData, mockHeadData } from './mock';
 
-// const papaparseOptions = {
-//   header: true,
-//   dynamicTyping: true,
-//   skipEmptyLines: true,
-//   transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
-// };
-
 function App() {
-  // const [rows, setRows] = useState([]);
-
-  // const handleForce = (data, fileInfo) => {
-  //   setRows(data)
-  // };
-
-  // const columns = Object.keys(mockData[0] || {}).map((key) => {
-  //   return {
-  //     key,
-  //     name: key
-  //   }
-  // })
 
   return (
     <>
-      {/* <div style={{marginBottom: "50px"}}>
-        <CSVReader
-          onFileLoaded={handleForce}
-          parserOptions={papaparseOptions}
-        />
-      </div> */}
-      <div>
-        <DataGrid columns={mockHeadData} rows={mockData} />;
+      <div className="ag-theme-alpine" style={{height: 400, width: '100%'}}>
+        <AgGridReact
+            rowData={mockData}
+            columnDefs={mockHeadData}>
+        </AgGridReact>
       </div>
     </>
   );
