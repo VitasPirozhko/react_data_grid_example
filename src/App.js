476ae14b41934cbc8b514e0bbb01a26ece36cb20
Вітас Пірожко <vitaspirozko@Vitass-Air.Dlink>
2023-03-15 11:32:15 +0200

    mock and prepare data

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 import 'react-data-grid/lib/styles.css';
 import DataGrid from 'react-data-grid';
-import CSVReader from "react-csv-reader";
-import { useState } from 'react';
+// import CSVReader from "react-csv-reader";
+// import { useState } from 'react';
+import { mockData, mockHeadData } from './mock';
 
-
-const papaparseOptions = {
-  header: true,
-  dynamicTyping: true,
-  skipEmptyLines: true,
-  transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
-};
+// const papaparseOptions = {
+//   header: true,
+//   dynamicTyping: true,
+//   skipEmptyLines: true,
+//   transformHeader: header => header.toLowerCase().replace(/\W/g, "_")
+// };
 
 function App() {
-  const [rows, setRows] = useState([]);
+  // const [rows, setRows] = useState([]);
 
-  const handleForce = (data, fileInfo) => {
-    setRows(data)
-  };
+  // const handleForce = (data, fileInfo) => {
+  //   setRows(data)
+  // };
 
-  const columns = Object.keys(rows[0] || {}).map((key) => {
-    return {
-      key,
-      name: key
-    }
-  })
-  console.log(rows);
+  // const columns = Object.keys(mockData[0] || {}).map((key) => {
+  //   return {
+  //     key,
+  //     name: key
+  //   }
+  // })
+
   return (
     <>
-      <div style={{marginBottom: "50px"}}>
+      {/* <div style={{marginBottom: "50px"}}>
         <CSVReader
           onFileLoaded={handleForce}
           parserOptions={papaparseOptions}
         />
-      </div>
+      </div> */}
       <div>
-        <DataGrid columns={columns} rows={rows} />;
+        <DataGrid columns={mockHeadData} rows={mockData} />;
       </div>
     </>
   );
